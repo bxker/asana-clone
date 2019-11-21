12a871506a637a6405104a00b210e49cc3f03231
bxker <jake.luke.baker@gmail.com>
2019-11-21 16:16:21 -0600

    fixing login to show onclick

--- src/components/Landing/LandingHeader/LandingHeader.tsx
+++ src/components/Landing/LandingHeader/LandingHeader.tsx
@@ -3,27 +3,34 @@ import {connect} from 'react-redux';
 import {getSession} from '../../../redux/reducers/userReducer';
 import { Link } from 'react-router-dom';
 import './styles/LandingHeader.scss'
-// import Login from '../../Login/Login';
+import Login from '../../Login/Login';
 
-const Landing: React.FC<{user_id: number, getSession: Function}> = (props) => {
-    return (
-        <div className="landing-header-parent">
-            <div className="landing-header-main">
-                <section className="landing-header-left">
-                    <img src="https://miro.medium.com/max/1904/1*BCe1HWc1Y0-lUVJo2BH6xQ.png" alt="logo" className="asana-logo"/>
-                    <h1>Why Asana? <img className="down-arrow" src="https://www.stickpng.com/assets/images/58f8bd170ed2bdaf7c128308.png"></img></h1>
-                    <h1>Solutions <img className="down-arrow" src="https://www.stickpng.com/assets/images/58f8bd170ed2bdaf7c128308.png"></img></h1>
-                    <h1>Resources <img className="down-arrow" src="https://www.stickpng.com/assets/images/58f8bd170ed2bdaf7c128308.png"></img></h1>
-                    <h1>Pricing</h1>
-                </section>
-                <section className="landing-header-right">
-                    <Link style={{textDecoration: 'none'}} to="/login"><h1>Log In</h1></Link>
-                    <Link style={{textDecoration: 'none'}} to="/register"><button>Try for free</button></Link>
-                </section>
+const Landing: React.FC< {user_id: any, getSession: any}> = (props) => {
+    const [toggle, setToggle] = React.useState(false)
+
+
+    
+        return (
+            <>
+            <div className="landing-header-parent">
+                <div className="landing-header-main">
+                    <section className="landing-header-left">
+                        <img src="https://miro.medium.com/max/1904/1*BCe1HWc1Y0-lUVJo2BH6xQ.png" alt="logo" className="asana-logo"/>
+                        <h1>Why Asana? <img className="down-arrow" src="https://www.stickpng.com/assets/images/58f8bd170ed2bdaf7c128308.png"></img></h1>
+                        <h1>Solutions <img className="down-arrow" src="https://www.stickpng.com/assets/images/58f8bd170ed2bdaf7c128308.png"></img></h1>
+                        <h1>Resources <img className="down-arrow" src="https://www.stickpng.com/assets/images/58f8bd170ed2bdaf7c128308.png"></img></h1>
+                        <h1>Pricing</h1>
+                    </section>
+                    <section className="landing-header-right">
+                        <h1 onClick={() => setToggle(true)}>Log In</h1>
+                        <Link style={{textDecoration: 'none'}} to="/register"><button>Try for free</button></Link>
+                    </section>
+                </div>
             </div>
-        </div>
-    )
-}
+                {toggle ? <Login setToggle={setToggle}/> : null}
+                </>
+        )
+    }
 
 const mapStateToProps = (reduxState: any)=> {
     return {
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -6,16 +6,20 @@ import './styles/Login.css';
 
 
 
-const Login: React.FC<{username: String, password: String, user_id: number, loginUser: Function, getSession: Function}> = (props) => {
+const Login: React.FC<{username?: String, password?: String, user_id?: number, loginUser?: Function, getSession?: Function, setToggle: Function}> = (props) => {
     const [username, setUsername] = React.useState(''); 
     const [password, setPassword] = React.useState('');
 
     React.useEffect(() => {
-        props.getSession();
+        if(props.getSession) {
+            props.getSession();
+        }
     }, [props])
 
     const login = () => {
-        props.loginUser({username, password})
+        if(props.loginUser) {
+            props.loginUser({username, password})
+        }
     }
 
     if(props.user_id){
@@ -25,8 +29,10 @@ const Login: React.FC<{username: String, password: String, user_id: number, logi
         return (
             <div className='login-parent'>
                 <div className="login-main">
+                    <h5 onClick={() => {props.setToggle(false)}}>X</h5>
                     <h1>Log In</h1>
                     <button className="google-login"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/1004px-Google_%22G%22_Logo.svg.png"></img>  -  Use Google Account</button>
+                    <h2>------  or -------</h2>
                     <input placeholder="Username" onChange={e => setUsername(e.target.value)}/>
                     <input type="password" placeholder="Password" onChange={e => setPassword(e.target.value)}/>
                     <button  className="login-button" onClick={() => login()}>Login</button>
